00485419aab931c3991216cc5efad4903faf6ebb
Greg Curl <greg.a.curl@gmail.com>
2021-05-28 14:27:33 -0400

    adding heroku domain to ENV

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@ end
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://auth_app.herokuapp.com'
+    origins 'https://peg-leg-api.herokuapp.com/'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, :key => '_peg_leg_app', domain: "peg_leg.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, :key => '_peg_leg_app', domain: "https://peg-leg-api.herokuapp.com/"
 else
   Rails.application.config.session_store :cookie_store, :key => '_peg_leg_app'
 end
