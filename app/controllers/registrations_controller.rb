83bd28c534513424ba7028023aecd5060e1d9cb5
Greg Curl <greg.a.curl@gmail.com>
2021-05-28 13:10:08 -0400

    commenting endpoints

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,11 @@
 class RegistrationsController < ApplicationController
+  # POST /registrations
   def create
     user = User.create!(
       email: params['user']['email'],
       password: params['user']['password'],
       password_confirmation: params['user']['password_confirmation']
     )
-
 
     if user
       session[:user_id] = user.id
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   include CurrentUserConcern
+  # POST /sessions
   def create
     user = User
             .find_by(email: params['user']['email'])
