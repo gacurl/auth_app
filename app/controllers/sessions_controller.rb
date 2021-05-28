ae01c5ab5315fb55835ab07385d97a5ea3a1b773
Greg Curl <greg.a.curl@gmail.com>
2021-05-28 13:06:55 -0400

    testing endpoints

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,40 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
   def create
     user = User
-            .find_by(email: params['user']['params'])
+            .find_by(email: params['user']['email'])
             .try(:authenticate, params['user']['password'])
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: :unauthorized }
+    end
   end
+
+    # GET /logged_in
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: { 
+        logged_in: false
+      }
+    end
+  end
+
+  # DELETE /logout
+  def logout
+    reset_session
+    render json: { status: :ok, logged_out: true }
+  end
+
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, :key => '_peg_leg_app', domain: "peg_leg.herokuapp.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, :key => '_peg_leg_app', domain: "peg_leg.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, :key => '_peg_leg_app'
+end
