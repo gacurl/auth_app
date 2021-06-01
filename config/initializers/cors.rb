8829ef5197777e5c17117f6c0b363eb21f4337cf
Greg Curl <greg.a.curl@gmail.com>
2021-06-01 16:51:08 -0400

    Testing with Fitz

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@ end
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://peg-leg-api.herokuapp.com/'
+    origins 'https://peg-leg.herokuapp.com/'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, :key => '_peg_leg_app', domain: "https://peg-leg-api.herokuapp.com/"
+  Rails.application.config.session_store :cookie_store, :key => '_peg_leg_app', domain: "https://peg-leg.herokuapp.com/"
 else
   Rails.application.config.session_store :cookie_store, :key => '_peg_leg_app'
 end
